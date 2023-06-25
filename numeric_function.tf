9fdaee145030c59e350595b2c25e8192af4aa750
anjali-2022 <anjali07pingale@gmail.com>
2023-06-25 13:10:15 +0530

    Create numeric_function.tf

---
+++ numeric_function.tf
@@ -0,0 +1,42 @@
+output "function_abs" {
+  value = "function abs value of -12.4 is ${abs(-12.4)}"
+}
+
+output "function_ceil" {
+  value = "function ceil value of 12.4 is ${ceil(12.4)}"
+}
+
+output "function_floor" {
+  value = "function floor value of 12.4 is ${floor(12.4)}"
+}
+
+output "function_log" {
+  value = "function log 4 of base 2 is ${log(16,2)}"
+}
+
+output "function_max" {
+  value = "function max of 12,4 is ${max(12,4)}"
+}
+
+output "function_min" {
+  value = "function min of 12,4 is ${min(12,4)}"
+}
+
+output "function_parse" {
+  value = "function parse of 0100101010 binary is ${parseint("0100101010",2)}"
+}
+
+output "function_power" {
+  value = "function power 2 of 3 is ${pow(3,2)}"
+}
+
+output "function_signum" {
+  value = "function signum value of -12 is ${signum(-12)}"
+}
+
+
+
+
+
+
+
